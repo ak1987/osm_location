6f726b56e64673fb5e25a29840f429f29120929e
ak <5795195+ak1987@users.noreply.github.com>
2025-09-05 00:22:08 +0500

    fixed icon

--- src/components/LocationMap.vue
+++ src/components/LocationMap.vue
@@ -2,6 +2,15 @@
 import { onMounted, onBeforeUnmount, ref } from 'vue';
 import { locationManager } from '@telegram-apps/sdk';
 import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 type CancelablePromise<T> = Promise<T> & { cancel?: () => void };
 
